9e4d85028e9f17afff5a349d5db9960ceb059736
G <giandriaditio46@gmail.com>
2026-01-19 15:19:46 +0700

    add file in

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['puppeteer'],
-  },
+  serverExternalPackages: ['puppeteer'],
   // Force Node.js runtime for scraper API routes
   generateBuildId: async () => {
     return 'build-' + Date.now()
